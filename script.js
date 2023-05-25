7b773e9eb1953bd6f8e21a404f4df87594662563
Abhishek Yadav <abhishek31820yadav@gmail.com>
2023-05-25 16:57:53 +0530

    hello

---
+++ script.js
@@ -0,0 +1,6 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const animation = document.createElement('div');
+    animation.classList.add('animation');
+    document.body.appendChild(animation);
+  });
+  
